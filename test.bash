ec5d15b5644790e855c6d55f712bcb4a60debbd2
HirokiHasegawa777 <s21C1094JW@s.chibakoudai.jp>
2022-12-01 01:59:27 +0900

    update test.bash

--- test.bash
+++ test.bash
@@ -2,12 +2,26 @@
 # © 2022 Hiroki Hasegawa
 # SPDX-FileCopyrightText: 2022 Hiroki Hasegawa
 # SPDX-License-Identifier: BSD-3-Clause
-import sys
 
-ans = 0
-for line in sys.stdin:
-  try:
-    ans += int(line)
-  except:
-    ans += float(line)
-print(ans)
+ng (){
+     echo NG at Line $1
+     res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+#out=$(echo あ | ./plus)
+#[ "$?" = 1 ] || ng ${LINENO}
+#[ "${out}" = "" ] || ng ${LINENO}
+
+#out=$(echo | ./plus)
+#[ "$?" = 1 ] || ng ${LINENO}
+#[ "${out}" = ""] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
